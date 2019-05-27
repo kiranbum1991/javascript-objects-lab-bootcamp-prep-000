d1e90820aa3c34ad9177f5d9cde6d2488e8e8e9f
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-27 10:08:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue (object, key, value) {
   Object.assign({}, obj);
-  Object.assign(object, { [key]: value })
   object[key] = value;
   return object;
 }
